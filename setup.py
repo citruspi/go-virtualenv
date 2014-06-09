5ce7e500184784aa99a4a84b279e1024d2dba30e
Mihir Singh <me@mihirsingh.com>
2014-06-08 21:36:44 -0700

    Setup python package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='go-virtualenv',
+    version='0.0.1',
+    author='Mihir Singh',
+    author_email='me@mihirsingh.com',
+    url='https://github.com/citruspi/go-virtualenv',
+    license='MIT License',
+    scripts=['scripts/go-virtualenv',]
+)
